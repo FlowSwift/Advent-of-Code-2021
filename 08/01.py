d6427de93f312b5a1c391649cf527ca5afc9076f
Flowy <flowy159@gmail.com>
2021-12-08 14:11:14 +0200

    Day 8

---
+++ 08/01.py
@@ -0,0 +1,7 @@
+with open("08/test_input.txt") as file_hnd:
+    count = []
+    for line in file_hnd:
+        patterns, digits = line.split(" | ")
+        digits = digits.split()
+        [count.append(num) for num in digits if len(num) == 2 or len(num) == 3 or len(num) == 4 or len(num) == 7]
+print(len(count))
