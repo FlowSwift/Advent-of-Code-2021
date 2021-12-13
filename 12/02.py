2a0dbf6e0c7b6d72bee947f8bd050a6f40bf4fbf
Flowy <flowy159@gmail.com>
2021-12-13 12:33:37 +0200

    Day 12

--- 12/02.py
+++ 12/02.py
@@ -3,7 +3,7 @@ https://adventofcode.com/2021/day/12
 """
 from collections import Counter
 map = {}  # graph of all nodes
-with open("12/input.txt") as file_hnd:
+with open("12/test_input.txt") as file_hnd:
     for line in file_hnd:
         nodes = line.strip().split("-")
         if nodes[0] not in map:
@@ -15,17 +15,26 @@ with open("12/input.txt") as file_hnd:
 
 visited = Counter()
 completed_path = 0
-def check_neighbors(current_node, visited, map):
+check = False
+def check_neighbors(current_node, visited, map, check):
     global completed_path
     for exit in current_node:
+        tmp_check = check
         if exit == "end":
             completed_path += 1
             continue
-        if (exit.islower() and visited[exit] >= 1) or exit == "start":
+        if exit == "start":
+            continue
+        if (exit.islower() and visited[exit] >= 1):
+            if tmp_check:
+                continue
+            tmp_check = True
+            tmp_visited = visited.copy()
+            check_neighbors(map[exit], tmp_visited, map, tmp_check)
             continue
         tmp_visited = visited.copy()
         tmp_visited[exit] += 1
-        check_neighbors(map[exit], tmp_visited, map)
+        check_neighbors(map[exit], tmp_visited, map, check)
 
-check_neighbors(map["start"], visited, map)
+check_neighbors(map["start"], visited, map, check)
 print(completed_path)
